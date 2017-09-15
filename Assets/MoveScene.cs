0d54d0f9f91a87648092f75926111d3bd7246163
Zach Roehl <zroehl19@gmail.com>
2017-09-15 13:50:00 -0400

    all get greedy folders

---
+++ Assets/MoveScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour 
+{
+	public GameObject guiObject;
+	public string loadLevel;
+
+	void Start()
+	{
+		guiObject.SetActive (false);
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			guiObject.SetActive (true);
+			if (guiObject.activeInHierarchy == true && Input.GetButtonDown ("Use"))
+			{
+				Application.LoadLevel (loadLevel);
+			}
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		guiObject.SetActive (false);
+	}
+}
